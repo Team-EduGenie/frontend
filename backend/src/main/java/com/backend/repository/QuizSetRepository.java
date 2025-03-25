6adea08e34276011ad1e449fe6a8f557773fa87b
platinouss <platinouss@gmail.com>
2025-03-25 13:54:51 +0900

    Rename: frontend 레포지토리 분리를 위한 파일 이동

--- backend/src/main/java/com/backend/repository/QuizSetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.repository;
-
-import com.backend.model.QuizSet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizSetRepository extends JpaRepository<QuizSet, Long> {
-} 
